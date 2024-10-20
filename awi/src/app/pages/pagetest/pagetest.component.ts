49d944a7b7ef1127f818d09dce6fe8217b32944d
Eugénio <baye.eugenio.egnb@gmail.com>
2024-10-21 00:14:36 +0200

    feat : Update /app architecture and add router to app

---
+++ awi/src/app/pages/pagetest/pagetest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagetest',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagetest.component.html',
+  styleUrl: './pagetest.component.css'
+})
+export class PagetestComponent {
+
+}
