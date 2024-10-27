7c34c4eb65bb92849aece85941d6f3f9c4f0ebe8
Eugénio <baye.eugenio.egnb@gmail.com>
2024-10-27 09:50:48 +0100

    feat : Use constants in header component

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,13 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { NAV_ITEMS, HEADER_ITEMS } from '@constants';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  navItems = NAV_ITEMS;
+  headerItems = HEADER_ITEMS;
+  
   isSidebarVisible = false;
 
   toggleSidebar() {
