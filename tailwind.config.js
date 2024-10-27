693c66c54dbdd3c6f6a0ad3bda91addfda3f1db7
Eugénio <baye.eugenio.egnb@gmail.com>
2024-10-27 16:30:07 +0100

    feat : Finish header template

--- tailwind.config.js
+++ tailwind.config.js
@@ -228,11 +228,14 @@ module.exports = {
       },
     },
   },
+  extend: {
+    
+  },
   plugins: [
     require("@catppuccin/tailwindcss")({
       // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
       // default is `false`, which means no prefix
       // which flavour of colours to use by default, in the `:root`
       defaultFlavour: "mocha",
-    }),  ],
+    }),],
 };
