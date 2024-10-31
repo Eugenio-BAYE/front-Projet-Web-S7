f22eafe715e5861d62af9d8ec4fe617b8babee85
Eugénio <baye.eugenio.egnb@gmail.com>
2024-10-31 16:58:25 +0100

    feat : Start user/create route

--- src/app/pages/user/user-create-page/user-create-page.component.ts
+++ src/app/pages/user/user-create-page/user-create-page.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
-import { UserCreateFormComponent } from './components/user-create-form/user-create-form.component';
-import { USER_CREATE_FORM_FIELDS } from './user-create-page.constants';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormComponent } from 'src/app/shared/components/form/form.component';
 
 @Component({
   selector: 'app-user-create-page',
   standalone: true,
   imports: [
-    UserCreateFormComponent
+    FormComponent,
   ],
   templateUrl: './user-create-page.component.html',
   styleUrl: './user-create-page.component.css'
 })
 export class UserCreatePageComponent {
-  formFields = USER_CREATE_FORM_FIELDS;
+  formFields = [
+    {
+      label: 'Username',
+      name: 'username',
+      type: 'text',
+      placeholder: 'Enter username',
+      required: true,
+      value: ''
+    },
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      placeholder: 'Enter email',
+      required: true,
+      value: ''
+    },
+    {
+      label: 'Role',
+      name: 'role',
+      type: 'select',
+      options: ['admin', 'manager'],
+      placeholder: 'Select a role',
+      required: true,
+      value: 'manager'
+    }
+  ];
 
   onFormSubmit(data: any) {
     console.log(data);
--- src/app/shared/components/input-field/input-field.component.ts
+++ src/app/shared/components/input-field/input-field.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.css'
 })
 export class InputFieldComponent {
-  @Input() field: any;
+  @Input() field: any; // Champ de configuration du formulaire
+  @Input() value: string = ''; // Valeur du champ
+  @Output() valueChange = new EventEmitter<any>(); // Émet un événement lors d'un changement de valeur
 
+  // Méthode appelée à chaque changement pour émettre la nouvelle valeur
+  onValueChange(value: any) {
+    this.valueChange.emit(value);
+  }
 }
