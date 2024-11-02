e3f5b622979d5554115d1ff74a1412a950806643
Eugénio <baye.eugenio.egnb@gmail.com>
2024-11-02 17:22:29 +0100

    feat(route) : Add a few routes to components   - Manager

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { GameDepositPageComponent } from './pages/game/game-deposit-page/game-deposit-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ManagePageComponent } from './pages/manage-page/manage-page.component';
+import { ManagerCreatePageComponent } from './pages/manager/manager-create-page/manager-create-page.component';
+import { ManagerManagePageComponent } from './pages/manager/manager-manage-page/manager-manage-page.component';
 
 export const routes: Routes = [
   // --- Main routes ---
@@ -28,17 +30,34 @@ export const routes: Routes = [
   // canActivate: [roleGuard],
   // data: { role: 'admin' | 'manager' }
   {
-    path:'login',
+    path: 'login',
     component: LoginPageComponent,
   },
   {
-    path:'manage',
+    path: 'manage',
     component: ManagePageComponent,
+    canActivate: [roleGuard],
+    data: { role: 'manager' },
+  },
+  {
+    path: 'manager',
+    canActivate: [roleGuard],
+    data: { role: 'admin' },
+    children: [
+      {
+        path: 'create',
+        component: ManagerCreatePageComponent
+      },
+      {
+        path: 'manage',
+        component: ManagerManagePageComponent
+      },
+    ],
   },
   {
     path: 'seller',
     canActivate: [roleGuard],
-    data: { role: 'admin' },
+    data: { role: 'manager' },
     children: [
       {
         path: '',
