d28cf2ba30e969782ef6aef531678817ce1d8b2d
Eugénio <baye.eugenio.egnb@gmail.com>
2024-11-02 17:09:28 +0100

    feat(manage) : Add main fonctionnalities to the manage list

--- src/app/pages/manage-page/manage-page.component.ts
+++ src/app/pages/manage-page/manage-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MANAGE_ITEMS } from './manage-page.constants';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-manage-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+  ],
   templateUrl: './manage-page.component.html',
   styleUrl: './manage-page.component.css'
 })
 export class ManagePageComponent {
+  manageItems = MANAGE_ITEMS;
 
 }
