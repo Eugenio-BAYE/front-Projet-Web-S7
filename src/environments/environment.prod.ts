c27e312e7cfaff1080a99d7e2fe1b20661fa5171
Eugénio <baye.eugenio.egnb@gmail.com>
2025-02-02 19:16:16 +0100

    fix : Update back url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://back-projet-web-s7-21ead7148147.herokuapp.com/api',
+  apiUrl: 'https://back-projet-web-s7-21ead7148147.herokuapp.com/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://back-projet-web-s7-21ead7148147.herokuapp.com/api',
+  apiUrl: 'https://back-projet-web-s7-21ead7148147.herokuapp.com/api',
   // 'http://back-projet-web-s7-21ead7148147.herokuapp.com/api' for the deployed back-end
 };
